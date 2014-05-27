71d607e1877cbb3579fb63519bc43fe630169c5a
Sean Ruel <sean.ruel@ahpcare.com>
2014-05-27 09:02:25 -0500

    Added click confirm directive and added to deleting users/clearing tasks

--- public/core.js
+++ public/core.js
@@ -1,6 +1,24 @@
 (function(){
 	var app = angular.module('ahpTasks', ['naturalSort']);
 
+	app.directive('ngConfirmClick', [
+	  function(){
+	    return {
+	      priority: -1,
+	      restrict: 'A',
+	      link: function(scope, element, attrs){
+	        element.bind('click', function(e){
+	          var message = attrs.ngConfirmClick;
+	          if(message && !confirm(message)){
+	            e.stopImmediatePropagation();
+	            e.preventDefault();
+	          }
+	        });
+	      }
+	    }
+	  }
+	]);
+
 	app.controller("UserController",function($http){
 		this.user={};
 		this.addUser = function(main,user){
